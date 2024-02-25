810b0fa4d841fc132294561c12db9c8712a10d7f
Jarne <dev@jarne.rocks>
2024-02-25 15:33:35 +0100

    Initial commit

---
+++ Commandoak/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Commandoak
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
