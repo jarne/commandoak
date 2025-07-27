5097c843a6a2788329ab6b93bf204fc6706e2df4
Jarne <dev@jarne.rocks>
2025-07-27 11:29:31 +0200

    Add position attribute and sort feature to commands

--- Commandoak/Model/Command.swift
+++ Commandoak/Model/Command.swift
@@ -8,13 +8,20 @@ import SwiftData
 
 @Model
 final class Command {
-    var name: String
-    var icon: String
-    var execute: String
+    static let defaultName = "New command"
+    static let defaultIcon = "🕹️"
+    static let defaultExecute = ""
+    static let defaultPosition = 0
+    
+    var name: String = defaultName
+    var icon: String = defaultIcon
+    var execute: String = defaultExecute
+    var position: Int = defaultPosition
 
-    init(name: String = "New command", icon: String = "🕹️", execute: String = "") {
+    init(name: String = defaultName, icon: String = defaultIcon, execute: String = defaultExecute, position: Int = defaultPosition) {
         self.name = name
         self.icon = icon
         self.execute = execute
+        self.position = position
     }
 }
